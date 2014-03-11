bded7fd20c01db467e2916abc6615b8376339f58
Jamie Countryman <jamie@jmcountryman.com>
2014-03-10 23:54:23 -0500

    Rearrange URLs

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,7 +5,7 @@ from reviews import views
 urlpatterns = patterns('',
 	url(r'^/$', views.index, name='index'),
 	url(r'^/create$', views.create_review, name='create_review'),
-	url(r'^/create/(?P<course_id>\w+)$', views.create_review_for, name='create_review_for'),
+	url(r'^/(?P<course_id>\w+)/create$', views.create_review_for, name='create_review_for'),
 	url(r'^/(?P<course_id>\w+)$', views.course_reviews, name='course_reviews'),
 	#url(r'^(?P<course_id>\w+)/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
 	)
